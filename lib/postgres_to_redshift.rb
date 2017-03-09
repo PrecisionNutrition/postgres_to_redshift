45a878eb2cb6140ac22eeaa64f451540da1ab3d3
Luke Galea <luke@ideaforge.org>
2017-03-09 17:25:51 -0500

    Fixed weird problems re: table fails

--- lib/postgres_to_redshift.rb
+++ lib/postgres_to_redshift.rb
@@ -76,7 +76,7 @@ class PostgresToRedshift
     tables_to_skip = ['events', 'versions', 'stripe_webhooks', 'string_versions', 'work_items_backup']
 	
     source_connection.exec("SELECT * FROM information_schema.tables WHERE table_schema = 'public' AND table_type = 'BASE TABLE'").map do |table_attributes|
-      table = Table.new(attributes: table_attributes)
+      table = Table.new(attributes: table_attributes)	  
       next if table.name =~ /^pg_/ || tables_to_skip.include?(table.name)
       table.columns = column_definitions(table)
       table
@@ -95,26 +95,29 @@ class PostgresToRedshift
     @bucket ||= s3.buckets[ENV['S3_DATABASE_EXPORT_BUCKET']]
   end
 
-  def copy_table(table)
-    tmpfile = Tempfile.new("psql2rs")     
+  def copy_table(table)	
+    tmpfile = Tempfile.new("psql2rs")
+	compressedTmpFile = Pathname.new("#{tmpfile.path}.bz2")
+	
 	File.chmod(0666, tmpfile) 
     chunk = 1
     bucket.objects.with_prefix("export/#{table.target_table_name}.psv.bz2").delete_all    	
 	
     begin
-      puts "Downloading #{table}"      
+      puts "Downloading #{table} into #{tmpfile.path}"      
 	  
 	  copy_command = "COPY (SELECT #{table.columns_for_copy} FROM #{table.name}) TO '#{tmpfile.path}' WITH DELIMITER '|'"
 	  
 	  source_connection.exec copy_command
 	  
-	  `/bin/bzip2 #{tmpfile.path} && mv #{tmpfile.path}.bz2 #{tmpfile.path}`
-	  
 	  File.chmod(0666, tmpfile) 
+	  
+	  system "/bin/bzip2 #{tmpfile.path}"
       
-      upload_table(table, tmpfile, chunk)	       
+      upload_table(table, compressedTmpFile, chunk)
     ensure            
       tmpfile.unlink
+	  compressedTmpFile.unlink
     end
   end
 
@@ -132,7 +135,7 @@ class PostgresToRedshift
     target_connection.exec("ALTER TABLE public.#{target_connection.quote_ident(table.target_table_name)} RENAME TO #{table.target_table_name}_updating")
 
     target_connection.exec("CREATE TABLE public.#{target_connection.quote_ident(table.target_table_name)} (#{table.columns_for_create})")
-
+	
     target_connection.exec("COPY public.#{target_connection.quote_ident(table.target_table_name)} FROM 's3://#{ENV['S3_DATABASE_EXPORT_BUCKET']}/export/#{table.target_table_name}.psv.bz2' CREDENTIALS 'aws_access_key_id=#{ENV['S3_DATABASE_EXPORT_ID']};aws_secret_access_key=#{ENV['S3_DATABASE_EXPORT_KEY']}' BZIP2 TRUNCATECOLUMNS ESCAPE DELIMITER as '|';")
 
     target_connection.exec("COMMIT;")
